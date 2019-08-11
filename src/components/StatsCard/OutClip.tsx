cc09ccab9e3482781364963be0d61cc1e21d7e12
Ott Tooming <OttTooming@gmail.com>
2019-08-11 13:05:37 +0300

    Add StasCard experiment with external clip-path

---
+++ src/components/StatsCard/OutClip.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import styled from "styled-components"
+
+export interface OutClipProps {}
+
+const Wrapper = styled.div`
+  position: absolute;
+  z-index: 1;
+  left: 35%;
+  right: 0;
+  bottom: 30px;
+  min-height: 100%;
+  background: rebeccapurple;
+  border-radius: 8px;
+`
+
+const Top = styled.div`
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  background: blue;
+  background: rebeccapurple;
+  height: 30px;
+  transform: skew(0, 20deg);
+  height: 190px;
+  top: -90px;
+  border-radius: 16px;
+  z-index: -1;
+`
+
+const OutClip: React.SFC<OutClipProps> = () => {
+  return (
+    <Wrapper>
+      Lorem
+      <Top />
+    </Wrapper>
+  )
+}
+
+export default OutClip
---
+++ src/components/StatsCard/StatsCard.tsx
@@ -0,0 +1,44 @@
+import * as React from "react"
+import styled from "styled-components"
+
+// @ts-ignore
+import StatsCardClip from "../../assets/statscard-clip.svg"
+import OutClip from "./OutClip"
+
+export interface StatsCardProps {}
+
+const HiddenStatsCardClip = styled(StatsCardClip)`
+  position: absolute;
+  width: 0;
+  height: 0;
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  z-index: 1;
+  max-width: 320px;
+  transform: rotate(-0.0000000001deg);
+`
+
+const Container = styled.div`
+  position: relative;
+  z-index: 2;
+  padding: 32px;
+  background: #fff;
+  border-radius: 8px;
+  min-height: 420px;
+  clip-path: polygon(0 -60%, 0 100%, 125% 100%);
+  clip-path: url(#statscard);
+`
+
+const StatsCard: React.SFC<StatsCardProps> = () => {
+  return (
+    <Wrapper>
+      <HiddenStatsCardClip />
+      <OutClip />
+      <Container>Lorem</Container>
+    </Wrapper>
+  )
+}
+
+export default StatsCard
