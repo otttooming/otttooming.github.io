97523f077c26b622e3d2f03818415ee7e63e9b50
Ott Tooming <OttTooming@gmail.com>
2019-08-20 02:05:43 +0300

    Add basic layout to Project template

--- src/templates/Project.tsx
+++ src/templates/Project.tsx
@@ -2,16 +2,24 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
+import styled from "styled-components"
 
 export interface PostProps {
   data: any
 }
 
+const Wrapper = styled.div`
+  max-width: 960px;
+  margin: 80px auto;
+`
+
 const Project: React.FC<PostProps> = ({ data: { mdx } }) => {
   return (
     <Layout>
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXRenderer>{mdx.body}</MDXRenderer>
+      <Wrapper>
+        <h1>{mdx.frontmatter.title}</h1>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </Wrapper>
     </Layout>
   )
 }
