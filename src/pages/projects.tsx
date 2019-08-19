e1f364cf52f5203f2934d4d9dd5cdb9185ef382c
Ott Tooming <OttTooming@gmail.com>
2019-08-20 01:58:08 +0300

    Add grid template area for listing projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,22 +1,38 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import { Card } from "@coterminous/ui"
+import GalleryImage from "../components/Gallery/GalleryImage"
+import styled from "styled-components"
+
+const Wrapper = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(256px, 1fr));
+  margin: 80px auto;
+  padding: 0;
+  max-width: 960px;
+`
+
+const Item = styled.li`
+  list-style: none;
+`
 
 const Projects = ({ data }) => {
   const { edges: posts } = data.allMdx
   return (
     <Layout>
-      <h1>Awesome MDX Blog</h1>
-      <ul>
+      <Wrapper>
         {posts.map(({ node: post }) => (
-          <li key={post.id}>
-            <Link to={post.fields.slug}>
-              <h2>{post.frontmatter.title}</h2>
-            </Link>
-            <p>{post.excerpt}</p>
-          </li>
+          <Item key={post.id}>
+            <Card content={<GalleryImage />}>
+              <Link to={post.fields.slug}>
+                <h2>{post.frontmatter.title}</h2>
+              </Link>
+              <p>{post.excerpt}</p>
+            </Card>
+          </Item>
         ))}
-      </ul>
+      </Wrapper>
     </Layout>
   )
 }
