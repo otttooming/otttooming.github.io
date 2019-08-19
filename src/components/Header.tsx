672751abb056e6ec0b3aa65663ce5cb9e1bab6d5
Ott Tooming <OttTooming@gmail.com>
2019-08-20 00:43:38 +0300

    Add link to project to main header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,19 @@ import * as React from "react"
 import styled from "styled-components"
 import { Wind } from "react-feather"
 import { theme } from "@coterminous/ui"
+import { Link as GatsbyLink } from "gatsby"
 
 const Wrapper = styled.header`
   display: flex;
   align-self: center;
+  justify-content: space-between;
   max-width: 960px;
   margin: 0 auto;
   padding: ${theme.space.l};
+`
+const Home = styled.div`
+  display: flex;
+  align-self: center;
 `
 
 const Heading = styled.h1`
@@ -17,10 +23,32 @@ const Heading = styled.h1`
   margin-left: 8px;
 `
 
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+`
+
+const Item = styled.li`
+  list-style: none;
+`
+
+const Link = styled(GatsbyLink)`
+  color: inherit;
+  text-decoration: none;
+`
+
 const Header = ({ siteTitle }) => (
-  <Wrapper style={{}}>
-    <Wind />
-    <Heading>OTTO</Heading>
+  <Wrapper>
+    <Home>
+      <Wind />
+      <Heading>OTTO</Heading>
+    </Home>
+
+    <List>
+      <Item>
+        <Link to="/projects">Projects</Link>
+      </Item>
+    </List>
   </Wrapper>
 )
 
