a6e9d8018b761afbf9226fdb35e2fa04152683d2
Ott Tooming <OttTooming@gmail.com>
2019-08-23 20:50:14 +0300

    Fix vertical  spacing for project listing items

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,7 +17,7 @@ const Item = styled.li`
   grid-template-columns: repeat(auto-fit, minmax(256px, 1fr));
   grid-gap: 32px;
   list-style: none;
-  margin-bottom: 256px;
+  margin-bottom: 196px;
   align-items: center;
 
   &:nth-child(even) {
