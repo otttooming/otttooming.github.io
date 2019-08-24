e09bdcf65dbf65cf39a5338c6a7f86798faf1dc0
Ott Tooming <OttTooming@gmail.com>
2019-08-24 11:09:03 +0300

    Add DESC sorting order for frontmatter___date in project listing

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -72,7 +72,7 @@ const Projects = ({ data }) => {
 }
 export const pageQuery = graphql`
   query projectIndex {
-    allMdx {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
