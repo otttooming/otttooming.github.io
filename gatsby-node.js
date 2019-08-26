63602a4c93f4688663bc4b917743b0483e6ea005
Ott Tooming <OttTooming@gmail.com>
2019-08-26 23:31:40 +0300

    Refactor createProjects to a separate function from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,8 @@
  */
 
 const { createFilePath } = require("gatsby-source-filesystem")
+const createProjects = require("./gatsby/node/createProjects")
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
   // We only want to operate on `Mdx` nodes. If we had content from a
@@ -30,36 +32,5 @@ const path = require("path")
 exports.createPages = async ({ graphql, actions, reporter }) => {
   // Destructure the createPage function from the actions object
   const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMdx {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-  if (result.errors) {
-    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
-  }
-  // Create blog post pages.
-  const posts = result.data.allMdx.edges
-  // We'll call `createPage` for each result
-  posts.forEach(({ node }, index) => {
-    createPage({
-      // This is the slug we created before
-      // (or `node.frontmatter.slug`)
-      path: node.fields.slug,
-      // This component will wrap our MDX content
-      component: path.resolve(`./src/templates/Project.tsx`),
-      // We can use the values in this context in
-      // our page layout component
-      context: { id: node.id },
-    })
-  })
+  createProjects(graphql, reporter, createPage)
 }
---
+++ gatsby/node/createProjects.js
@@ -0,0 +1,36 @@
+const path = require("path")
+
+module.exports = async function createProjects(graphql, reporter, createPage) {
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+  }
+  // Create blog post pages.
+  const posts = result.data.allMdx.edges
+  // We'll call `createPage` for each result
+  posts.forEach(({ node }, index) => {
+    createPage({
+      // This is the slug we created before
+      // (or `node.frontmatter.slug`)
+      path: node.fields.slug,
+      // This component will wrap our MDX content
+      component: path.resolve(`./src/templates/Project.tsx`),
+      // We can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
+    })
+  })
+}
