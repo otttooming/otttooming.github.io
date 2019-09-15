172cf400762633bdedbcfd473f9a976abcac1765
Ott Tooming <OttTooming@gmail.com>
2019-09-15 17:37:33 +0300

    Add dynamic row height for masonry layout in MasonryGallery

--- src/components/MasonryGallery/MasonryGallery.tsx
+++ src/components/MasonryGallery/MasonryGallery.tsx
@@ -1,31 +1,51 @@
-import * as React from "react"
 import styled from "styled-components"
+import * as React from "react"
 
 export interface MasonryGalleryProps {}
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(256px, 1fr));
+  grid-auto-flow: column;
   grid-gap: 16px;
-  grid-auto-rows: minmax(180px, auto);
-  grid-auto-flow: dense;
-  padding: 16px;
 `
 
 const Item = styled.div`
-  display: block;
-
-  > * {
-    display: block;
-  }
+  display: grid;
+  grid-gap: 16px;
+  grid-auto-rows: max-content;
 `
 
-const MasonryGallery: React.SFC<MasonryGalleryProps> = ({ children }) => {
+const MasonryGallery: React.FC<MasonryGalleryProps> = ({ children }) => {
+  const minWidth = 300
+  const cols = []
+  const ref = React.useRef(null)
+  const [numCols, setNumCols] = React.useState(3)
+
+  const calcNumCols = () =>
+    setNumCols(Math.floor(ref.current.offsetWidth / minWidth))
+
+  const createCols = () => {
+    for (let i = 0; i < numCols; i++) cols[i] = []
+
+    React.Children.forEach(children, (child, i) =>
+      cols[i % numCols].push(child)
+    )
+  }
+
+  React.useEffect(() => {
+    calcNumCols()
+    window.addEventListener("resize", calcNumCols)
+    return () => window.removeEventListener("resize", calcNumCols)
+  })
+  createCols()
+
   return (
-    <Wrapper>
-      {React.Children.map(children, c => (
-        <Item>{c}</Item>
-      ))}
+    <Wrapper ref={ref}>
+      {Array(numCols)
+        .fill(null)
+        .map((el, i) => (
+          <Item key={i}>{cols[i]}</Item>
+        ))}
     </Wrapper>
   )
 }
