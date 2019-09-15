a073abec4ada3b0e0c36d1ea63cac700c3944cf5
Ott Tooming <OttTooming@gmail.com>
2019-09-15 22:56:35 +0300

    Updates

--- component---src-pages-page-2-js-3a4ef9cd142cb4971137.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{240:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(90),c=a(235),r=a(237);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-3a4ef9cd142cb4971137.js.map
--- component---src-pages-page-3-tsx-bd737c099a822d95018a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{241:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n),o=a(90),c=a(235),r=a(237),u=a(50);t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.a,{to:"/"},"Go back to the homepage"),"dsads",l.a.createElement(u.Button,null,"fdfs"))}}}]);
-//# sourceMappingURL=component---src-pages-page-3-tsx-bd737c099a822d95018a.js.map
