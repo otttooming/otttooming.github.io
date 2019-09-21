8f5d855116d2c3e5624c52e789c76f4cb62b40e9
Ott Tooming <OttTooming@gmail.com>
2019-09-21 16:00:53 +0300

    Fix Gallery responsivness for mobile devices

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -9,12 +9,18 @@ export interface GalleryProps {}
 const Wrapper = styled.div`
   display: grid;
   align-items: stretch;
-  grid-gap: 25px;
-  grid-template-rows: repeat(14, auto);
+  grid-gap: 16px;
   max-width: 960px;
-  min-height: 480px;
   margin: 0 auto;
   padding: 0 ${theme.space.s};
+
+  grid-template-rows: repeat(14, minmax(24px, 1fr));
+  grid-template-columns: repeat(9, minmax(16px, 1fr));
+
+  @media (min-width: 960px) {
+    grid-template-rows: repeat(14, minmax(16px, 1fr));
+    grid-template-columns: repeat(22, minmax(16px, 1fr));
+  }
 `
 
 const Item = styled.div`
@@ -24,7 +30,7 @@ const Item = styled.div`
     display: none;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:first-of-type {
       display: block;
       grid-column: 1/7;
@@ -37,7 +43,7 @@ const Item = styled.div`
     grid-row: 1/8;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:nth-of-type(2) {
       grid-column: 7/11;
     }
@@ -48,7 +54,7 @@ const Item = styled.div`
     grid-row: 2/8;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:nth-of-type(3) {
       grid-column: 11/17;
     }
@@ -58,7 +64,7 @@ const Item = styled.div`
     display: none;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:nth-of-type(4) {
       display: block;
       grid-column: 17/23;
@@ -71,7 +77,7 @@ const Item = styled.div`
     grid-row: 8/14;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:nth-of-type(5) {
       grid-column: 7/13;
     }
@@ -82,7 +88,7 @@ const Item = styled.div`
     grid-row: 8/15;
   }
 
-  @media (min-width: 40em) {
+  @media (min-width: 960px) {
     &:nth-of-type(6) {
       grid-column: 13/17;
     }
