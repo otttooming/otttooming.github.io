083a41ed8d7aa590a8b0f5b651a8378aad4103eb
Ott Tooming <OttTooming@gmail.com>
2019-10-04 23:39:01 +0300

    Fix missing description from IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
 import styled from "styled-components"
 import { GridItem, theme, Heading, Button, Link } from "@coterminous/ui"
 import { GitHub, Linkedin, Twitter } from "react-feather"
@@ -75,7 +75,7 @@ const Text = styled.p`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" description="" />
     <Segment>
       <Grid>
         <GridItem>
