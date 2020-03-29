4d63013229f80585ac93154c8455750b98187f80
Ott Tooming <OttTooming@gmail.com>
2020-03-29 17:03:37 +0300

    Fix Index page styles to use proper spacing with chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,14 @@ import Gallery from "../components/Gallery/Gallery"
 import HexGrid from "../components/HexGrid/HexGrid"
 import Coop from "../components/Coop/Coop"
 import { theme } from "../utils/theme"
-import { Button, Link, Heading, Box } from "@chakra-ui/core"
+import { Link, Heading, Box, Text as ChakraText } from "@chakra-ui/core"
 
 const StyledImage = styled(Image)`
   border-radius: 8px;
   width: 100%;
+  align-content: center;
+  display: flex;
+  justify-content: center;
 `
 
 const Grid = styled.section`
@@ -35,10 +38,14 @@ const AltGrid = styled.section`
   margin: 0 auto;
 `
 
-const PlainGrid = styled.section`
-  max-width: 960px;
-  margin: var(--space-xl) auto;
-`
+const PlainGrid = ({ ...restProps }) => (
+  <Box
+    as="section"
+    maxWidth="960px"
+    margin="var(--space-xl) auto"
+    {...restProps}
+  />
+)
 
 const CenterImage = styled.div`
   align-content: center;
@@ -46,10 +53,9 @@ const CenterImage = styled.div`
   justify-content: center;
 `
 
-const StyledSubHeading = styled.h2`
-  font-weight: ${theme.fontWeight.normal};
-  margin: 0;
-`
+const Subheading = ({ ...restProps }) => (
+  <ChakraText as="h2" fontWeight={theme.fontWeight.normal} {...restProps} />
+)
 
 const Segment = styled.div``
 
@@ -60,13 +66,7 @@ const SmallType = styled.p`
   font-size: 14px;
 `
 
-const Center = styled.div`
-  text-align: center;
-`
-
-const Text = styled.p`
-  margin: 0;
-`
+const Text = ({ ...restProps }) => <ChakraText mt="8px" {...restProps} />
 
 const OutLink = ({ ...restProps }) => (
   <Link mt={theme.space.s} display="flex" {...restProps} />
@@ -82,24 +82,27 @@ const IndexPage = () => (
             Hi, I am <strong>Ott</strong>
           </Heading>
 
-          <StyledSubHeading>
+          <Subheading>
             Front-end developer/<strong>student</strong>
-          </StyledSubHeading>
-          <p>
+          </Subheading>
+
+          <Text mt="16px">
             This is something to collate all my thoughts and gather the
             documentation I have lingering around
-          </p>
+          </Text>
 
-          <Text>
+          <Text mt="32px">
             <OutLink href="https://github.com/otttooming">
               <GitHub /> github.com/otttooming
             </OutLink>
           </Text>
+
           <Text>
             <OutLink href="https://www.linkedin.com/in/otttooming/">
               <Linkedin /> linkedin.com/in/otttooming
             </OutLink>
           </Text>
+
           <Text>
             <OutLink href="https://twitter.com/otttooming">
               <Twitter /> twitter.com/otttooming
@@ -107,20 +110,18 @@ const IndexPage = () => (
           </Text>
         </Box>
 
-        <Box>
-          <CenterImage>
-            <StyledImage />
-          </CenterImage>
-        </Box>
+        <CenterImage>
+          <StyledImage />
+        </CenterImage>
       </Grid>
     </Segment>
 
     <Segment>
       <PlainGrid>
         <SmallType>Passions</SmallType>
-        <StyledSubHeading>
+        <Subheading>
           <strong>Animal welfare</strong> and my cats and dogs specifically
-        </StyledSubHeading>
+        </Subheading>
       </PlainGrid>
 
       <Gallery />
@@ -133,25 +134,24 @@ const IndexPage = () => (
 
         <Box>
           <SmallType>Experience</SmallType>
-          <StyledSubHeading>
+          <Subheading>
             <strong>Tech stack</strong> that I currently use
-          </StyledSubHeading>
-          <p>
+          </Subheading>
+
+          <Text>
             Passionate about TypeScript, React, NodeJS, GraphQL and statically
             typed languages in general.
-          </p>
+          </Text>
         </Box>
       </AltGrid>
     </Segment>
 
     <Segment>
-      <PlainGrid>
-        <Center>
-          <SmallType>Industry</SmallType>
-          <StyledSubHeading>
-            Some <strong>amazing companies </strong> I've worked with
-          </StyledSubHeading>
-        </Center>
+      <PlainGrid textAlign="center">
+        <SmallType>Industry</SmallType>
+        <Subheading>
+          Some <strong>amazing companies </strong> I've worked with
+        </Subheading>
 
         <Coop />
       </PlainGrid>
