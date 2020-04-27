c44b3fcc228cc7e4a05163aa8a0ee86a0ea62ee1
Ott Tooming <OttTooming@gmail.com>
2020-04-27 22:20:18 +0300

    Refactor Header to use Box component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,59 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
 import { Wind, BookOpen, User } from 'react-feather';
 import { navigate } from 'gatsby';
-import { Button } from '@chakra-ui/core';
+import { Button, Box, BoxProps } from '@chakra-ui/core';
 import { theme } from '../utils/theme';
 import DarkMode from './DarkMode/DarkMode';
 
-const Wrapper = styled.header`
-  display: flex;
-  align-self: center;
-  justify-content: space-between;
-  max-width: 960px;
-  margin: 0 auto;
-  padding: ${theme.space.l};
-`;
-const Home = styled(Button)`
-  font-size: 18px;
-  background: transparent;
-`;
-
-const List = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`;
-
-const Item = styled.li`
-  display: flex;
-  align-self: center;
-  list-style: none;
-  margin-right: 24px;
-
-  &:last-of-type {
-    margin-right: 0;
-  }
-`;
-
-const StyledButton = styled(Button)`
-  background: transparent;
-`;
+const Item: React.FC<BoxProps> = ({ ...restProps }) => (
+  <Box
+    display="flex"
+    alignSelf="center"
+    listStyleType="none"
+    ml="24px"
+    {...restProps}
+  />
+);
 
 const Header = ({ siteTitle }) => (
-  <Wrapper>
-    <Home leftIcon={Wind} variant="link" onClick={() => navigate('/')}>
+  <Box
+    display="flex"
+    alignSelf="center"
+    justifyContent="space-between"
+    maxWidth="960px"
+    m="0 auto"
+    p={theme.space.l}
+  >
+    <Button leftIcon={Wind} variant="link" onClick={() => navigate('/')}>
       OTTO
-    </Home>
+    </Button>
 
-    <List>
+    <Box display="flex" m="0" p="0">
       <Item>
-        <StyledButton
+        <Button
           leftIcon={User}
           variant="ghost"
           onClick={() => navigate('/about/me')}
         >
           About
-        </StyledButton>
+        </Button>
       </Item>
+
       <Item>
-        <StyledButton
+        <Button
           leftIcon={BookOpen}
           variant="ghost"
           onClick={() => navigate('/projects')}
         >
           Projects
-        </StyledButton>
+        </Button>
       </Item>
+
       <Item>
         <DarkMode />
       </Item>
-    </List>
-  </Wrapper>
+    </Box>
+  </Box>
 );
 
 Header.defaultProps = {
