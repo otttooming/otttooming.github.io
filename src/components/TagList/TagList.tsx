50c4fc9f99e667142c7efc078d659590a9e50e8f
Ott Tooming <OttTooming@gmail.com>
2020-05-10 17:26:08 +0300

    Refactor TagList to use Box component

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -1,24 +1,13 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
 import { theme } from '../../utils/theme';
-import { Button } from '@chakra-ui/core';
-import { Tag } from './tagMap';
+import { Button, Box } from '@chakra-ui/core';
+import { Tag as TagProps } from './tagMap';
 import { Link as GatsbyLink } from 'gatsby';
 import { getIsExternalLink } from '../../utils/getIsExternalLink';
 
 export interface TagListProps {
-  tags: Tag[];
+  tags: TagProps[];
 }
-
-const Wrapper = styled.ul`
-  margin: ${theme.space.m} auto;
-  padding: 0;
-`;
-
-const Item = styled.li`
-  list-style: none;
-  display: inline-flex;
-`;
 
 const link = (to: string) => ({ ...restProps }) => {
   const isExternal = getIsExternalLink(to);
@@ -32,25 +21,25 @@ const link = (to: string) => ({ ...restProps }) => {
   return <GatsbyLink to={to} {...restProps} />;
 };
 
-const Tag: React.FC<Tag> = ({ title, url }) => {
+const Tag: React.FC<TagProps> = ({ title, url }) => {
   const rightIcon = getIsExternalLink(url) ? 'external-link' : null;
 
   return (
-    <Item>
+    <Box as="li" listStyleType="none" display="inline-flex">
       <Button as={link(url)} mr={theme.space.s} size="xs" rightIcon={rightIcon}>
         {title}
       </Button>
-    </Item>
+    </Box>
   );
 };
 
 const TagList: React.FC<TagListProps> = ({ tags }) => {
   return (
-    <Wrapper>
+    <Box as="ul" my={theme.space.m} mx="auto" p="0">
       {tags.map((item) => (
         <Tag key={item.title} {...item} />
       ))}
-    </Wrapper>
+    </Box>
   );
 };
 
