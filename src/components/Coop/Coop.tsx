e5eb4d9aaadb9348c2e7cab4d3805f5608129b45
Ott Tooming <OttTooming@gmail.com>
2020-05-15 16:11:29 +0300

    Fix DOM pollution within Company list

--- src/components/Coop/Coop.tsx
+++ src/components/Coop/Coop.tsx
@@ -5,37 +5,55 @@ import Voog from '../../assets/logo-voog.svg';
 import Gtap from '../../assets/logo-gtap.svg';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
+import { Box, BoxProps } from '@chakra-ui/core';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
 `;
 
-const Item = styled.div<{ type?: string }>`
-  background: #25292c;
-  height: 200px;
-  width: 300px;
-  margin-right: 8px;
-  transform: rotateX(65deg) rotateZ(-45deg) translate3d(0, 0, 0);
-  border-radius: 16px;
-  display: flex;
-  align-items: center;
-    justify-content: center;
-    margin-left: -100px;
-    transition: transform 500ms;
-    position: relative;
-    z-index: 1;
-    color: #fff;
+const getCompanyBgColor = (type?: string) => {
+  if (type === 'iglu') {
+    return '#1797d4';
+  }
 
+  if (type === 'voog') {
+    return '#443DF6';
+  }
 
-    ${({ type }) => (type === 'iglu' ? 'background: #1797d4;' : '')}
-    ${({ type }) => (type === 'voog' ? 'background: #443DF6;' : '')}
-    ${({ type }) => (type === 'gtap' ? 'background: #00bf9c;' : '')}
+  if (type === 'gtap') {
+    return '#00bf9c';
+  }
 
-    /* &:hover {
-        transform: rotateX(65deg) rotateZ(-45deg) translate3d(62px, 62px, 62px);
-    } */
-`;
+  return '#25292c';
+};
+
+const Company: React.FC<BoxProps & { type?: string }> = ({
+  type,
+  ...restProps
+}) => {
+  const backgroundColor = getCompanyBgColor(type);
+
+  return (
+    <Box
+      backgroundColor={backgroundColor}
+      height="200px"
+      width="300px"
+      marginRight="8px"
+      transform="rotateX(65deg) rotateZ(-45deg) translate3d(0, 0, 0)"
+      borderRadius="16px"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      marginLeft="-100px"
+      transition="transform 500ms"
+      position="relative"
+      zIndex={1}
+      color="#fff"
+      {...restProps}
+    />
+  );
+};
 
 const cardCommon = css`
   width: 100%;
@@ -65,18 +83,21 @@ const GtapCard = styled(Gtap)`
 const Coop: React.FC = () => {
   return (
     <Wrapper>
-      <Item type="gtap">
+      <Company type="gtap">
         <GtapCard />
-      </Item>
-      <Item type="voog">
+      </Company>
+
+      <Company type="voog">
         <VoogCard />
-      </Item>
-      <Item type="iglu">
+      </Company>
+
+      <Company type="iglu">
         <IgluCard />
-      </Item>
-      <Item>
+      </Company>
+
+      <Company>
         <PipedriveCard />
-      </Item>
+      </Company>
     </Wrapper>
   );
 };
