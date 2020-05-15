015585a858e39e12b011755f447843ce88cc165d
Ott Tooming <OttTooming@gmail.com>
2020-05-15 16:54:31 +0300

    Refactor Company card to use Box component

--- src/components/Coop/Coop.tsx
+++ src/components/Coop/Coop.tsx
@@ -3,14 +3,7 @@ import Pipedrive from '../../assets/logo-pipedrive.svg';
 import Iglu from '../../assets/logo-iglu.svg';
 import Voog from '../../assets/logo-voog.svg';
 import Gtap from '../../assets/logo-gtap.svg';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 import { Box, BoxProps } from '@chakra-ui/core';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-`;
 
 const getCompanyBgColor = (type?: string) => {
   if (type === 'iglu') {
@@ -55,50 +48,29 @@ const Company: React.FC<BoxProps & { type?: string }> = ({
   );
 };
 
-const cardCommon = css`
-  width: 100%;
-  max-height: 100%;
-  max-width: 200px;
-`;
-
-const PipedriveCard = styled(Pipedrive)`
-  ${cardCommon}
-  max-width: 250px;
-`;
-
-const IgluCard = styled(Iglu)`
-  ${cardCommon}
-
-  max-width: 150px;
-`;
-const VoogCard = styled(Voog)`
-  ${cardCommon}
-`;
-
-const GtapCard = styled(Gtap)`
-  ${cardCommon}
-  max-width: 250px;
-`;
+const Card: React.FC<BoxProps> = ({ ...restProps }) => {
+  return <Box width="100%" maxHeight="100%" maxWidth="200px" {...restProps} />;
+};
 
 const Coop: React.FC = () => {
   return (
-    <Wrapper>
+    <Box display="flex" flexDirection="row-reverse">
       <Company type="gtap">
-        <GtapCard />
+        <Card as={Gtap} maxWidth="250px" />
       </Company>
 
       <Company type="voog">
-        <VoogCard />
+        <Card as={Voog} />
       </Company>
 
       <Company type="iglu">
-        <IgluCard />
+        <Card as={Iglu} maxWidth="150px" />
       </Company>
 
       <Company>
-        <PipedriveCard />
+        <Card as={Pipedrive} maxWidth="250px" />
       </Company>
-    </Wrapper>
+    </Box>
   );
 };
 
