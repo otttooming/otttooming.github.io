0c786b6fd1a0ce595160a3afe09716c275302c24
Ott Tooming <OttTooming@gmail.com>
2020-05-27 14:31:09 +0300

    Refactor Coop companies to remove distinct background for uniform list

--- src/components/Coop/Coop.tsx
+++ src/components/Coop/Coop.tsx
@@ -6,32 +6,14 @@ import Gtap from '../../assets/logo-gtap.svg';
 import { Box, BoxProps, Link as ChakraLink, LinkProps } from '@chakra-ui/core';
 
 const Company: React.FC<BoxProps> = ({ ...restProps }) => {
-  return (
-    <Box
-      as="li"
-      height="200px"
-      width="300px"
-      marginRight="8px"
-      transform="rotateX(65deg) rotateZ(-45deg) translate3d(0, 0, 0)"
-      borderRadius="16px"
-      marginLeft="-100px"
-      transition="transform 500ms"
-      position="relative"
-      zIndex={1}
-      color="#fff"
-      {...restProps}
-    />
-  );
+  return <Box as="li" {...restProps} />;
 };
 
 const Link: React.FC<LinkProps> = ({ ...restProps }) => {
   return (
     <ChakraLink
-      position="absolute"
-      top={0}
-      bottom={0}
-      right={0}
-      left={0}
+      height="128px"
+      mx="16px"
       display="flex"
       alignItems="center"
       justifyContent="center"
@@ -43,7 +25,7 @@ const Link: React.FC<LinkProps> = ({ ...restProps }) => {
 };
 
 const Card: React.FC<BoxProps> = ({ ...restProps }) => {
-  return <Box width="100%" maxHeight="100%" maxWidth="200px" {...restProps} />;
+  return <Box width="200px" maxHeight="100%" {...restProps} />;
 };
 
 const Coop: React.FC = () => {
@@ -51,28 +33,31 @@ const Coop: React.FC = () => {
     <Box
       as="ol"
       display="flex"
+      justifyContent="center"
       flexDirection="row-reverse"
+      flexWrap="wrap"
       listStyleType="none"
+      mt="64px"
     >
-      <Company backgroundColor="#00bf9c">
+      <Company>
         <Link href="https://play.ee/">
           <Card as={Gtap} maxWidth="250px" />
         </Link>
       </Company>
 
-      <Company backgroundColor="#443DF6">
+      <Company>
         <Link href="https://www.voog.com/">
           <Card as={Voog} />
         </Link>
       </Company>
 
-      <Company backgroundColor="#1797d4">
+      <Company>
         <Link href="https://www.iglu.ee/">
           <Card as={Iglu} maxWidth="150px" />
         </Link>
       </Company>
 
-      <Company backgroundColor="#25292c">
+      <Company>
         <Link href="https://www.pipedrive.com/">
           <Card as={Pipedrive} maxWidth="250px" />
         </Link>
