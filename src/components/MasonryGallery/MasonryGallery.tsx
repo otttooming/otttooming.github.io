944e88449a09f496af589fa1cb2884076d3e9839
Ott Tooming <OttTooming@gmail.com>
2020-05-29 01:33:51 +0300

    Revert "Refactor MasonryGallery to use CSS columns"
    This reverts commit 150a2ed36a3030be6c30557ace86ebce9a7a3cde.

--- src/components/MasonryGallery/MasonryGallery.tsx
+++ src/components/MasonryGallery/MasonryGallery.tsx
@@ -3,29 +3,63 @@ import * as React from 'react';
 import { theme } from '../../utils/theme';
 
 const Wrapper = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 16px;
   margin-top: 80px;
   margin-bottom: 80px;
   max-width: 1280px !important;
-  columns: 4 300px;
-  column-gap: 16px;
+`;
+
+const Column = styled.div`
+  display: grid;
+  grid-gap: 16px;
+  grid-auto-rows: max-content;
 `;
 
 const Item = styled.div`
-  display: inline-block;
-  width: 100%;
-  margin-top: 16px;
   overflow: hidden;
   border-radius: ${theme.borderRadius.s};
 `;
 
 const MasonryGallery: React.FC = ({ children }) => {
+  const childrenCount = React.Children.count(children);
+
+  const minWidth = 300;
+  const cols = [];
+  const ref = React.useRef(null);
+  const [numCols, setNumCols] = React.useState(3);
+
+  const calcNumCols = () => {
+    const possibleColumns = Math.floor(ref.current.offsetWidth / minWidth);
+    const optimalColumns =
+      possibleColumns > childrenCount ? childrenCount : possibleColumns;
+
+    setNumCols(optimalColumns);
+  };
+
+  const createCols = () => {
+    for (let i = 0; i < numCols; i++) cols[i] = [];
+
+    React.Children.forEach(children, (child, i) =>
+      cols[i % numCols].push(<Item key={i}>{child}</Item>)
+    );
+  };
+
+  React.useEffect(() => {
+    calcNumCols();
+    window.addEventListener('resize', calcNumCols);
+    return () => window.removeEventListener('resize', calcNumCols);
+  });
+  createCols();
+
   return (
-    <Wrapper>
-      {React.Children.map(children, (child, i) => (
-        <div>
-          <Item key={i}>{child}</Item>
-        </div>
-      ))}
+    <Wrapper ref={ref}>
+      {Array(numCols)
+        .fill(null)
+        .map((el, i) => (
+          <Column key={i}>{cols[i]}</Column>
+        ))}
     </Wrapper>
   );
 };
