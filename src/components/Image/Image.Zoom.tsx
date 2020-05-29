6330e4c0a0654668225ed69e48e7c01fdeb93e60
Ott Tooming <OttTooming@gmail.com>
2020-05-29 20:13:04 +0300

    Refactor Image.Zoom to use Box component

--- src/components/Image/Image.Zoom.tsx
+++ src/components/Image/Image.Zoom.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import ImageWithZoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
+import { Box } from '@chakra-ui/core';
 
-function ImageZoom(props) {
-  const image = {
-    ...props,
-    className: 'Image__Zoom',
-    style: {
-      display: 'block',
-      margin: '0 auto',
-      width: '100%',
-      borderRadius: '5px',
-    },
-  };
-
+function ImageZoom({ style, ...restProps }) {
   return (
     <ImageWithZoom>
-      <img {...image} />
+      <Box {...restProps} as="img" position="relative" />
     </ImageWithZoom>
   );
 }
