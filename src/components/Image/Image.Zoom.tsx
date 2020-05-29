e1ee076e42bdeb18f960bf0e16474567d429eba0
Ott Tooming <OttTooming@gmail.com>
2020-05-29 20:15:31 +0300

    Refactor Image.Zoom component to use arrow function

--- src/components/Image/Image.Zoom.tsx
+++ src/components/Image/Image.Zoom.tsx
@@ -3,12 +3,15 @@ import ImageWithZoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 import { Box } from '@chakra-ui/core';
 
-function ImageZoom({ style, ...restProps }) {
+const ImageZoom: React.FC<React.HTMLAttributes<HTMLImageElement>> = ({
+  style,
+  ...restProps
+}) => {
   return (
     <ImageWithZoom>
       <Box {...restProps} as="img" position="relative" />
     </ImageWithZoom>
   );
-}
+};
 
 export default ImageZoom;
