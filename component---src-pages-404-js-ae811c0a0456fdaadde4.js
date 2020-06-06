064632c702d83e39c2702d079debe60aebc0a426
Ott Tooming <OttTooming@gmail.com>
2020-06-06 19:53:35 +0300

    Updates

--- component---src-pages-404-js-ae811c0a0456fdaadde4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,n){"use strict";n.r(e);n("q1tI");var u=n("9Dj+"),o=n("ytpH"),s=n("qKvR");e.default=function(){return Object(s.d)(u.a,null,Object(s.d)(o.a,{title:"404: Not found"}),Object(s.d)("h1",null,"NOT FOUND"),Object(s.d)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-ae811c0a0456fdaadde4.js.map
