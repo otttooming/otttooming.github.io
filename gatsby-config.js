601ac4ddd77dcad87b232b28b14fb141fd11ebe3
Ott Tooming <OttTooming@gmail.com>
2020-06-07 19:40:22 +0300

    Remove default values from gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,13 +68,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Personal portfolio - Ott`,
+        short_name: `Ott portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/wind.svg`, // This path is relative to the root of the site.
+        legacy: false,
       },
     },
     {
