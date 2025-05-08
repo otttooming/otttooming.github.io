2f37c7e56d665ad50aa837afdc86b095155720d3
Ott Tooming <OttTooming@gmail.com>
2025-05-09 00:15:13 +0300

    refactor: remove wrapStyle as deprecated

--- src/components/Image/Image.Zoom.tsx
+++ src/components/Image/Image.Zoom.tsx
@@ -2,18 +2,8 @@ import * as React from 'react';
 import ImageWithZoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-const ImageZoom: React.FC<React.HTMLAttributes<HTMLImageElement>> = ({
-  children,
-  ...restProps
-}) => {
-  return (
-    <ImageWithZoom
-      wrapElement="div"
-      wrapStyle={{ display: 'block', width: '100%', ...restProps }}
-    >
-      {children}
-    </ImageWithZoom>
-  );
+const ImageZoom = ({ children }) => {
+  return <ImageWithZoom wrapElement="div">{children}</ImageWithZoom>;
 };
 
 export default ImageZoom;
