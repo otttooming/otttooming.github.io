8e0ba73594f1ae0ad3b4b710c8c54ab60b8ba453
Ott Tooming <OttTooming@gmail.com>
2025-05-16 00:39:48 +0300

    refactor: standardize quotes in gatsby-config.js
    Replaced template literals with single quotes for consistency
    throughout the gatsby-config.js file. This change improves
    readability and maintains a uniform coding style.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title: `Personal portfolio - Ott`,
-    description: `This is something to collate all my thoughts.`,
-    author: `@OttTooming`,
+    title: 'Personal portfolio - Ott',
+    description: 'This is something to collate all my thoughts.',
+    author: '@OttTooming',
   },
   plugins: [
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/projects`,
-        name: `projects`,
+        name: 'projects',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/about`,
-        name: `about`,
+        name: 'about',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/persons`,
-        name: `persons`,
+        name: 'persons',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/technologies`,
-        name: `technologies`,
+        name: 'technologies',
       },
     },
     {
@@ -57,23 +57,23 @@ module.exports = {
               withWebp: true,
             },
           },
-          `gatsby-remark-copy-linked-files`,
+          'gatsby-remark-copy-linked-files',
         ],
       },
     },
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Personal portfolio - Ott`,
-        short_name: `Ott portfolio`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/wind.svg`, // This path is relative to the root of the site.
+        name: 'Personal portfolio - Ott',
+        short_name: 'Ott portfolio',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/wind.svg', // This path is relative to the root of the site.
         legacy: false,
       },
     },
