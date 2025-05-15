26e3344ac7fc6b606ae4a4b30e3eaf3b3350d992
Ott Tooming <OttTooming@gmail.com>
2025-05-16 00:23:26 +0300

    refactor: simplify Logo component height handling
    Removed the getHeight function and directly set heights for specific
    logos within the Logo component. This improves readability and
    maintains consistent height values for logos.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -11,51 +11,33 @@ export type LogoProps = {
   name: string;
 } & BoxProps;
 
-const getHeight = (type: string) => {
-  if (type === 'iglu') {
-    return '32px';
-  }
-
-  if (type === 'pethealth') {
-    return '32px';
-  }
-
-  if (type === 'khk') {
-    return '24px';
-  }
-
-  return '16px';
-};
-
 const Icon: React.FC<BoxProps> = ({ ...restProps }) => {
   return <Box height="16px" maxWidth="100%" {...restProps} />;
 };
 
 const Logo: React.FC<LogoProps> = ({ name, ...restProps }) => {
-  const height = getHeight(name);
-
   if (name === 'voog') {
-    return <Icon as={Voog} height={height} {...restProps} />;
+    return <Icon as={Voog} {...restProps} />;
   }
 
   if (name === 'iglu') {
-    return <Icon as={Iglu} height={height} {...restProps} />;
+    return <Icon as={Iglu} height="32px" {...restProps} />;
   }
 
   if (name === 'gtap') {
-    return <Icon as={Gtap} height={height} {...restProps} />;
+    return <Icon as={Gtap} {...restProps} />;
   }
 
   if (name === 'pipedrive') {
-    return <Icon as={Pipedrive} height={height} {...restProps} />;
+    return <Icon as={Pipedrive} {...restProps} />;
   }
 
   if (name === 'pethealth') {
-    return <Icon as={Pethealth} height={height} {...restProps} />;
+    return <Icon as={Pethealth} height="32px" {...restProps} />;
   }
 
   if (name === 'khk') {
-    return <Icon as={Khk} height={height} {...restProps} />;
+    return <Icon as={Khk} height="24px" {...restProps} />;
   }
 
   return null;
