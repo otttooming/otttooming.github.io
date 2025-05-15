394c31cf999d60ec658dd54e1afea73aa77aca75
Ott Tooming <OttTooming@gmail.com>
2025-05-15 23:43:42 +0300

    chore: convert Image component to TSX

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
+import * as React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import *as React from 'react';
 
 const Image = ({ ...restProps }) => {
   const data = useStaticQuery(graphql`
