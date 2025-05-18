6acb4870e1707fc5fd3cf942dd061170985e035d
Ott Tooming <OttTooming@gmail.com>
2025-05-18 22:43:50 +0300

    fix: update SVG paths to absolute URLs
    Changed SVG import paths from relative to absolute URLs for
    consistency and to ensure correct loading of logo assets across
    different environments.

--- src/components/Logo/CompanyLogos.tsx
+++ src/components/Logo/CompanyLogos.tsx
@@ -1,35 +1,35 @@
 export const Voog = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="Voog" {...rest}>
-    <use href="./logo-voog.svg#logo-voog" />
+    <use href="/logo-voog.svg#logo-voog" />
   </svg>
 );
 
 export const Pipedrive = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="Pipedrive" {...rest}>
-    <use href="./logo-pipedrive.svg#logo-pipedrive" />
+    <use href="/logo-pipedrive.svg#logo-pipedrive" />
   </svg>
 );
 
 export const Iglu = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="Iglu" {...rest}>
-    <use href="./logo-iglu.svg#logo-iglu" />
+    <use href="/logo-iglu.svg#logo-iglu" />
   </svg>
 );
 
 export const Gtap = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="gotoAndPlay" {...rest}>
-    <use href="./logo-gtap.svg#logo-gtap" />
+    <use href="/logo-gtap.svg#logo-gtap" />
   </svg>
 );
 
 export const Pethealth = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="Pethealth" {...rest}>
-    <use href="./logo-pethealth.svg#logo-pethealth" />
+    <use href="/logo-pethealth.svg#logo-pethealth" />
   </svg>
 );
 
 export const Khk = ({ ...rest }: React.ComponentProps<'svg'>) => (
   <svg role="img" aria-label="Tartu KHK" {...rest}>
-    <use href="./logo-khk.svg#logo-khk" />
+    <use href="/logo-khk.svg#logo-khk" />
   </svg>
 );
