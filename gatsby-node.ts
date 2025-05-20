d1790404d992adf8c083eb0d7db8e534d9c26a4e
Ott Tooming <OttTooming@gmail.com>
2025-05-20 22:58:26 +0300

    refactor: remove unnecessary comments in gatsby-node
    Cleaned up comments in the onCreateNode and createPages functions.
    This improves code readability by eliminating redundant explanations.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -14,21 +14,13 @@ const templates = {
 
 export function onCreateNode({ node, actions, getNode }) {
   const { createNodeField } = actions;
-  // We only want to operate on `Mdx` nodes. If we had content from a
-  // remote CMS we could also check to see if the parent node was a
-  // `File` node here
   if (node.internal.type === 'Mdx') {
     const value = createFilePath({ node, getNode });
     const directory = node.internal.contentFilePath.split('/').reverse()[2];
 
     createNodeField({
-      // Name of the field you are adding
       name: 'slug',
-      // Individual MDX node
       node,
-      // Generated value based on filepath with "blog" prefix. We
-      // don't need a separating "/" before the value because
-      // createFilePath returns a path with the leading "/".
       value: `/${directory}${value}`,
     });
   }
@@ -57,13 +49,8 @@ export async function createPages({ graphql, actions: { createPage } }) {
     const directory = node.internal.contentFilePath.split('/').reverse()[2];
 
     createPage({
-      // This is the slug we created before
-      // (or 'node.frontmatter.slug')
       path: `${directory}/${node.frontmatter.slug}`,
-      // This component will wrap our MDX content
       component: `${templates[directory]}?__contentFilePath=${node.internal.contentFilePath}`,
-      // We can use the values in this context in
-      // our page layout component
       context: { id: node.id },
     });
   });
