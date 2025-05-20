7801fee5acd1a87fa30aad4541887cdf0a0f35f0
Ott Tooming <OttTooming@gmail.com>
2025-05-20 22:54:43 +0300

    refactor: move createProjects logic into createPages
    This change removes the createProjects function and integrates its logic
    directly into the createPages function. The onCreateNode function is
    now exported and retains its functionality for handling Mdx nodes,
    ensuring the slug field is created correctly.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -12,7 +12,32 @@ const templates = {
   technologies: path.resolve('./src/pages/Route404.tsx'),
 };
 
-async function createProjects(graphql, reporter, createPage) {
+export function onCreateNode({ node, actions, getNode }) {
+  const { createNodeField } = actions;
+  // We only want to operate on `Mdx` nodes. If we had content from a
+  // remote CMS we could also check to see if the parent node was a
+  // `File` node here
+  if (node.internal.type === 'Mdx') {
+    const value = createFilePath({ node, getNode });
+    const directory = node.internal.contentFilePath.split('/').reverse()[2];
+
+    createNodeField({
+      // Name of the field you are adding
+      name: 'slug',
+      // Individual MDX node
+      node,
+      // Generated value based on filepath with "blog" prefix. We
+      // don't need a separating "/" before the value because
+      // createFilePath returns a path with the leading "/".
+      value: `/${directory}${value}`,
+    });
+  }
+}
+
+export async function createPages({ graphql, actions, reporter }) {
+  // Destructure the createPage function from the actions object
+  const { createPage } = actions;
+
   const result = await graphql(`
     query {
       allMdx {
@@ -49,32 +74,4 @@ async function createProjects(graphql, reporter, createPage) {
       context: { id: node.id },
     });
   });
-}
-
-export function onCreateNode({ node, actions, getNode }) {
-  const { createNodeField } = actions;
-  // We only want to operate on `Mdx` nodes. If we had content from a
-  // remote CMS we could also check to see if the parent node was a
-  // `File` node here
-  if (node.internal.type === 'Mdx') {
-    const value = createFilePath({ node, getNode });
-    const directory = node.internal.contentFilePath.split('/').reverse()[2];
-
-    createNodeField({
-      // Name of the field you are adding
-      name: 'slug',
-      // Individual MDX node
-      node,
-      // Generated value based on filepath with "blog" prefix. We
-      // don't need a separating "/" before the value because
-      // createFilePath returns a path with the leading "/".
-      value: `/${directory}${value}`,
-    });
-  }
-}
-
-export async function createPages({ graphql, actions, reporter }) {
-  // Destructure the createPage function from the actions object
-  const { createPage } = actions;
-  await createProjects(graphql, reporter, createPage);
 }
