71b6562a0e4c371b46d06bba5bc09d4e454bec48
Ott Tooming <OttTooming@gmail.com>
2025-05-25 15:40:50 +0300

    refactor: migrate Gallery component to vanilla-extract styles
    Replaced styled-components with vanilla-extract for the Gallery
    component's styles. This change improves style management and
    consistency across the application. The styles are now defined
    in a separate CSS file, enhancing maintainability and readability.

---
+++ src/components/Gallery/Gallery.css.ts
@@ -0,0 +1,103 @@
+import { style, globalStyle } from '@vanilla-extract/css';
+import { theme } from '../../utils/theme';
+
+export const wrapper = style({
+  display: 'grid',
+  alignItems: 'stretch',
+  gap: '16px',
+  width: '100%',
+  maxWidth: '960px',
+  margin: '0 auto',
+  gridTemplateRows: 'repeat(14, minmax(24px, 1fr))',
+  gridTemplateColumns: 'repeat(9, minmax(16px, 1fr))',
+  '@media': {
+    '(min-width: 960px)': {
+      gridTemplateRows: 'repeat(14, minmax(16px, 1fr))',
+      gridTemplateColumns: 'repeat(22, minmax(16px, 1fr))',
+    },
+  },
+});
+
+export const item = style({
+  position: 'relative',
+  selectors: {
+    '&:first-of-type': {
+      display: 'none',
+      '@media': {
+        '(min-width: 960px)': {
+          display: 'block',
+          gridColumn: '1/7',
+          gridRow: '3/12',
+        },
+      },
+    },
+    '&:nth-of-type(2)': {
+      gridColumn: '1/5',
+      gridRow: '1/8',
+      '@media': {
+        '(min-width: 960px)': {
+          gridColumn: '7/11',
+        },
+      },
+    },
+    '&:nth-of-type(3)': {
+      gridColumn: '5/10',
+      gridRow: '2/8',
+      '@media': {
+        '(min-width: 960px)': {
+          gridColumn: '11/17',
+        },
+      },
+    },
+    '&:nth-of-type(4)': {
+      display: 'none',
+      '@media': {
+        '(min-width: 960px)': {
+          display: 'block',
+          gridColumn: '17/23',
+          gridRow: '4/10',
+        },
+      },
+    },
+    '&:nth-of-type(5)': {
+      gridColumn: '1/6',
+      gridRow: '8/14',
+      '@media': {
+        '(min-width: 960px)': {
+          gridColumn: '7/13',
+        },
+      },
+    },
+    '&:nth-of-type(6)': {
+      gridColumn: '6/10',
+      gridRow: '8/15',
+      '@media': {
+        '(min-width: 960px)': {
+          gridColumn: '13/17',
+        },
+      },
+    },
+  },
+});
+
+export const itemContainer = style({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  bottom: 0,
+  top: 0,
+  overflow: 'hidden',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: theme.borderRadius.m,
+});
+
+globalStyle(`${itemContainer} > *`, {
+  borderRadius: theme.borderRadius.m,
+  position: 'relative',
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+  flex: 1,
+});
--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,118 +1,6 @@
-import styled from '@emotion/styled';
 import { graphql, useStaticQuery } from 'gatsby';
-import { theme } from '../../utils/theme';
 import { GatsbyImage } from 'gatsby-plugin-image';
-
-const Wrapper = styled.div`
-  display: grid;
-  align-items: stretch;
-  grid-gap: 16px;
-  width: 100%;
-  max-width: 960px;
-  margin: 0 auto;
-
-  grid-template-rows: repeat(14, minmax(24px, 1fr));
-  grid-template-columns: repeat(9, minmax(16px, 1fr));
-
-  @media (min-width: 960px) {
-    grid-template-rows: repeat(14, minmax(16px, 1fr));
-    grid-template-columns: repeat(22, minmax(16px, 1fr));
-  }
-`;
-
-const Item = styled.div`
-  position: relative;
-
-  &:first-of-type {
-    display: none;
-  }
-
-  @media (min-width: 960px) {
-    &:first-of-type {
-      display: block;
-      grid-column: 1/7;
-      grid-row: 3/12;
-    }
-  }
-
-  &:nth-of-type(2) {
-    grid-column: 1/5;
-    grid-row: 1/8;
-  }
-
-  @media (min-width: 960px) {
-    &:nth-of-type(2) {
-      grid-column: 7/11;
-    }
-  }
-
-  &:nth-of-type(3) {
-    grid-column: 5/10;
-    grid-row: 2/8;
-  }
-
-  @media (min-width: 960px) {
-    &:nth-of-type(3) {
-      grid-column: 11/17;
-    }
-  }
-
-  &:nth-of-type(4) {
-    display: none;
-  }
-
-  @media (min-width: 960px) {
-    &:nth-of-type(4) {
-      display: block;
-      grid-column: 17/23;
-      grid-row: 4/10;
-    }
-  }
-
-  &:nth-of-type(5) {
-    grid-column: 1/6;
-    grid-row: 8/14;
-  }
-
-  @media (min-width: 960px) {
-    &:nth-of-type(5) {
-      grid-column: 7/13;
-    }
-  }
-
-  &:nth-of-type(6) {
-    grid-column: 6/10;
-    grid-row: 8/15;
-  }
-
-  @media (min-width: 960px) {
-    &:nth-of-type(6) {
-      grid-column: 13/17;
-    }
-  }
-`;
-
-const ItemContainer = styled.div`
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: ${theme.borderRadius.m};
-
-  > * {
-    border-radius: ${theme.borderRadius.m};
-    position: relative;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    flex: 1;
-  }
-`;
+import * as styles from './Gallery.css';
 
 const Gallery = () => {
   const data = useStaticQuery(graphql`
@@ -140,7 +28,7 @@ const Gallery = () => {
   `);
 
   return (
-    <Wrapper>
+    <div className={styles.wrapper}>
       {data.images.edges.map(
         ({
           node: {
@@ -151,18 +39,18 @@ const Gallery = () => {
           },
         }) => {
           return (
-            <Item key={slug}>
-              <ItemContainer>
+            <div key={slug} className={styles.item}>
+              <div className={styles.itemContainer}>
                 <GatsbyImage
                   image={image.childImageSharp.gatsbyImageData}
                   alt={slug}
                 />
-              </ItemContainer>
-            </Item>
+              </div>
+            </div>
           );
         },
       )}
-    </Wrapper>
+    </div>
   );
 };
 
