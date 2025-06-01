c38e89af877c7ee79c116ffb5190a802d0404faf
Ott Tooming <OttTooming@gmail.com>
2025-06-01 23:09:34 +0300

    refactor: simplify TagList component and link handling
    Removed the custom link handling and external link detection logic
    from the TagList component. Updated the rendering of tags to use
    the Link component directly for improved clarity and maintainability.

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -1,51 +1,19 @@
 import { Box } from '@chakra-ui/react';
-import { Link as GatsbyLink } from 'gatsby';
-import { ExternalLink } from 'react-feather';
-import { getIsExternalLink } from '../../utils/getIsExternalLink';
 import { theme } from '../../utils/theme.css';
 import type { Tag as TagProps } from './tagMap';
-import { Button } from '../Button/Button';
+import { Link } from '../Link/Link';
 
 export interface TagListProps {
   tags: TagProps[];
 }
 
-const link =
-  (to: string) =>
-  ({ ...restProps }) => {
-    const isExternal = getIsExternalLink(to);
-
-    if (isExternal) {
-      return (
-        <a href={to} target="_blank" rel="noopener noreferrer" {...restProps} />
-      );
-    }
-
-    return <GatsbyLink to={to} {...restProps} />;
-  };
-
-const Tag = ({ title, url }: TagProps) => {
-  const rightIcon = getIsExternalLink(url) ? (
-    <Box ml={theme.space.s}>
-      <ExternalLink width="14px" height="14px" />
-    </Box>
-  ) : null;
-
-  return (
-    <Box as="li" listStyleType="none" display="inline-flex">
-      <Button as={link(url)}>
-        {title}
-        {rightIcon}
-      </Button>
-    </Box>
-  );
-};
-
 const TagList = ({ tags }: TagListProps) => {
   return (
     <Box as="ul" my={theme.space.m} mx="auto" p="0">
-      {tags.map((item) => (
-        <Tag key={item.title} {...item} />
+      {tags.map(({ title, url }) => (
+        <Box key={title} as="li" listStyleType="none" display="inline-flex">
+          <Link href={url}>{title}</Link>
+        </Box>
       ))}
     </Box>
   );
