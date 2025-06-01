22f11b336a354592a0c2cfc862d470706437b658
Ott Tooming <OttTooming@gmail.com>
2025-06-01 23:49:57 +0300

    refactor: remove Button component and associated styles
    This commit deletes the Button component and its CSS styles.
    The removal simplifies the codebase and eliminates unused
    dependencies, streamlining the component structure.

--- src/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import { button } from './Button.css';
-
-export const Button = ({
-  ...props
-}: Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'>) => {
-  return <button className={button} {...props} />;
-};
