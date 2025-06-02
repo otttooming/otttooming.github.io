a40367703bdfc40d0e21d691963e8cc255a3cebc
Ott Tooming <OttTooming@gmail.com>
2025-06-02 23:56:37 +0300

    refactor: TagList to use Set intersection for filtering tags
    - Changed the tags prop type from TagProps[] to string[].
    - Added mapping prop to retrieve title and url for each tag.

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -1,17 +1,19 @@
-import type { Tag as TagProps } from './tagMap';
 import { Link } from '../Link/Link';
 import { tagList, tagListItem } from './TagList.css';
 
 export interface TagListProps {
-  tags: TagProps[];
+  tags: string[];
+  mapping: Record<string, { title: string; url: string }>;
 }
 
-const TagList = ({ tags }: TagListProps) => {
+const TagList = ({ tags, mapping }: TagListProps) => {
+  const items = new Set(tags).intersection(new Set(Object.keys(mapping)));
+
   return (
     <ul className={tagList}>
-      {tags.map(({ title, url }) => (
-        <li key={title} className={tagListItem}>
-          <Link href={url}>{title}</Link>
+      {Array.from(items).map((key) => (
+        <li key={key} className={tagListItem}>
+          <Link href={mapping[key].url}>{mapping[key].title}</Link>
         </li>
       ))}
     </ul>
