c042d2fa38848acedcd181be96e5b3fc8630682e
Ott Tooming <OttTooming@gmail.com>
2025-06-06 00:00:29 +0300

    refactor: remove ChakraProvider from Provider component
    This change simplifies the Provider component by removing the
    ChakraProvider wrapper, allowing it to directly return the
    ColorModeProvider. This aligns with the goal of reducing
    dependencies on Chakra UI components.

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,10 +1,5 @@
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
 
 export function Provider(props: ColorModeProviderProps) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  );
+  return <ColorModeProvider {...props} />;
 }
