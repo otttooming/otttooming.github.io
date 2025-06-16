96a3c739c095d6738aeaa754781040a79750e8c5
Ott Tooming <OttTooming@gmail.com>
2025-06-16 23:57:27 +0300

    refactor(_app.tsx): remove ThemeProvider for ColorModeProvider
    Eliminate the ThemeProvider component to simplify the app structure.
    Retain ColorModeProvider for theme management while enhancing clarity.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,15 @@
 import type { AppProps } from 'next/app';
 import './_app.css';
-import { ThemeProvider } from 'next-themes';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 import { ColorModeProvider } from '../components/ui/color-mode';
 
 export default function Aoo({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      disableTransitionOnChange
-      enableSystem
-      enableColorScheme
-    >
-      <ColorModeProvider>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <ColorModeProvider>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ColorModeProvider>
   );
 }
