c8ce573ad0ccf6be3ab8b4afe076766e5a189124
Ott Tooming <OttTooming@gmail.com>
2025-06-18 23:34:06 +0300

    refactor(_app.tsx): add montserrat font loading with next/font/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import './_app.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 import { ColorModeProvider } from '../components/ui/color-mode';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function Aoo({ Component, pageProps }: AppProps) {
   return (
-    <ColorModeProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </ColorModeProvider>
+    <main className={montserrat.className}>
+      <ColorModeProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ColorModeProvider>
+    </main>
   );
 }
