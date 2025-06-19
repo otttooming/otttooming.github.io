464590965b29f26d064b96c480e1f3411f0e77b0
Ott Tooming <OttTooming@gmail.com>
2025-06-19 23:23:28 +0300

    refactor(Gallery): migrate from Gatsby to Next.js Image
    Replaced GatsbyImage and GraphQL query with static image imports
    and Next.js Image component for improved performance and
    simplification of the Gallery component.

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,55 +1,34 @@
-import { graphql, useStaticQuery } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
 import * as styles from './Gallery.css';
+import bella from '../../../content/persons/bella/featured.jpeg';
+import bobby from '../../../content/persons/bobby/featured.jpeg';
+import gustav from '../../../content/persons/gustav/featured.jpeg';
+import max from '../../../content/persons/max/featured.jpeg';
+import ossu from '../../../content/persons/ossu/featured.jpeg';
+import tondu from '../../../content/persons/tondu/featured.jpeg';
+
+import Image from 'next/image';
+
+const images = [
+  ['Bella', bella],
+  ['Bobby', bobby],
+  ['Gustav', gustav],
+  ['Max', max],
+  ['Ossu', ossu],
+  ['Tondu', tondu],
+] as const;
 
 const Gallery = () => {
-  const data = useStaticQuery(graphql`
-    query GalleryQuery {
-      images: allMdx(
-        filter: {internal: {contentFilePath: { regex: "/persons/" }}}
-        limit: 6
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-              featured {
-                image {
-                  childImageSharp {
-                    gatsbyImageData(width: 245, layout: CONSTRAINED)
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
-
   return (
     <div className={styles.wrapper}>
-      {data.images.edges.map(
-        ({
-          node: {
-            frontmatter: {
-              slug,
-              featured: { image },
-            },
-          },
-        }) => {
-          return (
-            <div key={slug} className={styles.item}>
-              <div className={styles.itemContainer}>
-                <GatsbyImage
-                  image={image.childImageSharp.gatsbyImageData}
-                  alt={slug}
-                />
-              </div>
+      {images.map(([alt, src]) => {
+        return (
+          <div key={alt} className={styles.item}>
+            <div className={styles.itemContainer}>
+              <Image src={src} alt={alt} />
             </div>
-          );
-        },
-      )}
+          </div>
+        );
+      })}
     </div>
   );
 };
