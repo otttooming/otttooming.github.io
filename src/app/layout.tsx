0c89e3410a06533a1f3c20c2ac40e561978fecae
Ott Tooming <OttTooming@gmail.com>
2025-06-20 22:01:21 +0300

    refactor(layout): move to next.js app based routing

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './layout.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer/Footer';
+import { ColorModeProvider } from '../components/ui/color-mode';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <ColorModeProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ColorModeProvider>
+      </body>
+    </html>
+  );
+}
