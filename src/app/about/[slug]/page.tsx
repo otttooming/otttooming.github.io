4549a30a50721d270bab9688727076a1edccb561
Ott Tooming <OttTooming@gmail.com>
2025-07-06 13:02:33 +0300

    feat(about): use next-mdx-remote-client for MDX support

---
+++ src/app/about/[slug]/page.tsx
@@ -0,0 +1,84 @@
+import { evaluate, MDXRemote } from 'next-mdx-remote-client/rsc';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import TagList from '../../../components/TagList/TagList';
+import { socialTags } from '../../../constants/social';
+import { About } from './page.components';
+import * as styles from './page.css';
+import Image from 'next/image';
+import MDXComponents from '../../../components/MDXComponents/MDXComponents';
+
+type Frontmatter = {
+  title: string;
+  date: string;
+  slug: string;
+  featured: {
+    illustration: string;
+    width: number;
+    height: number;
+    alt: string;
+  };
+  tags: string[];
+};
+
+async function getPost(slug: string) {
+  const source = await fs.readFile(
+    path.join(process.cwd(), `public/content/about/${slug}`, 'index.mdx'),
+    'utf-8',
+  );
+  const { frontmatter } = await evaluate<Frontmatter>({
+    source,
+    options: {
+      parseFrontmatter: true,
+    },
+  });
+  return {
+    source,
+    slug,
+    excerpt: '',
+    frontmatter,
+  };
+}
+
+export default async function AboutPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const { source, frontmatter } = await getPost(slug);
+  const {
+    title,
+    tags,
+    featured: { illustration, width, height, alt },
+  } = frontmatter;
+
+  return (
+    <>
+      <div className={styles.coverWrapper}>
+        <Image
+          className={styles.coverImage}
+          src={`/content/about/${slug}/${illustration}`}
+          width={width}
+          height={height}
+          alt={alt}
+          loading="lazy"
+        />
+      </div>
+
+      <About.Wrapper>
+        <h1>{frontmatter.title}</h1>
+
+        <TagList tags={tags} mapping={socialTags} />
+      </About.Wrapper>
+
+      <About.Wrapper>
+        <MDXRemote
+          options={{ parseFrontmatter: true }}
+          components={MDXComponents}
+          source={source}
+        />
+      </About.Wrapper>
+    </>
+  );
+}
