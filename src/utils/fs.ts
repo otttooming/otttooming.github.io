949a439a64627c4f7bb6e7e2e9d2e43b283b62be
Ott Tooming <OttTooming@gmail.com>
2025-07-07 20:22:41 +0300

    fix(fs): ensure no hidden system files are read, i.e. .DS_Store

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -3,7 +3,13 @@ import path from 'node:path';
 
 export async function readDirNames(relativePath: string) {
   try {
-    return await readdir(path.join(process.cwd(), relativePath));
+    return await readdir(path.join(process.cwd(), relativePath), {
+      withFileTypes: true,
+    }).then((files) =>
+      files
+        .filter((file) => file.isDirectory())
+        .map((directory) => directory.name),
+    );
   } catch (error) {
     console.error(`Error reading directory ${relativePath}:`, error);
   }
