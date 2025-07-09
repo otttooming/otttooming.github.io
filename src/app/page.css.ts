c411da621f948cc5de8728274293fcb3b68614e1
Ott Tooming <OttTooming@gmail.com>
2025-07-10 00:06:09 +0300

    fix(portrait): prevent overflowing parent container

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -107,6 +107,8 @@ export const socialIcon = style({
 });
 
 export const portrait = style({
+  width: '100%',
+  height: 'auto',
   borderRadius: '70% 30% 30% 70% / 60% 40% 60% 40%',
   boxShadow: '5px 25px 40px rgba(0, 0, 0, 0.2)',
 });
