a82cbcf509f4a3ea9694e8ccadfd71f256c94214
Ott Tooming <OttTooming@gmail.com>
2025-07-13 21:18:13 +0300

    chore(mdx): remove @next/mdx dependency as using next-mdx-remote-client

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import createMDX from '@next/mdx';
-
-const withMDX = createMDX({
-  extension: /\.mdx$/,
-  options: {
-    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-    rehypePlugins: [],
-  },
-});
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-export default withVanillaExtract(
-  withMDX({
-    pageExtensions: ['mdx', 'ts', 'tsx'],
-  }),
-);
+export default withVanillaExtract();
