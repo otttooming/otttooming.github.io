684209d350133d0b8f9cd8ed58fbf4836c2bfc30
Ott Tooming <OttTooming@gmail.com>
2025-07-20 14:57:22 +0300

    fix(ImageZoom): add types for children prop

--- src/components/Image/Image.Zoom.tsx
+++ src/components/Image/Image.Zoom.tsx
@@ -1,7 +1,8 @@
+import { PropsWithChildren } from 'react';
 import ImageWithZoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-const ImageZoom = ({ children }) => {
+const ImageZoom = ({ children }: PropsWithChildren) => {
   return <ImageWithZoom wrapElement="div">{children}</ImageWithZoom>;
 };
 
