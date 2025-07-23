9daab216596c13d4b4ca6cbfe3e4b3ed3d966a21
Ott Tooming <OttTooming@gmail.com>
2025-07-23 18:16:21 +0300

    fix(layout): apply paragraph margin only to vertical axis for reduced specificity

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -37,7 +37,8 @@ globalStyle('h3', {
 globalStyle('h1, h2, h3, h4, h5, p', {
   fontWeight: theme.fontWeight.normal,
   lineHeight: theme.lineHeight.s,
-  margin: 0,
+  marginTop: 0,
+  marginBottom: 0,
 });
 
 globalStyle('strong', {
