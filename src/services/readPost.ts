25909f0d7d8d5e64798eee4f013bd0055909111b
Ott Tooming <OttTooming@gmail.com>
2025-07-23 22:30:09 +0300

    add(service): extract readPost to separate generic function for reusability

---
+++ src/services/readPost.ts
@@ -0,0 +1,20 @@
+import { evaluate } from 'next-mdx-remote-client/rsc';
+import { readSourceFile } from '../utils/fs';
+
+export async function readPost<Frontmatter extends Record<string, unknown>>(
+  relativePath: string,
+) {
+  const source = await readSourceFile(relativePath, 'index.mdx');
+
+  const { frontmatter } = await evaluate<Frontmatter>({
+    source,
+    options: {
+      parseFrontmatter: true,
+    },
+  });
+
+  return {
+    source,
+    frontmatter,
+  };
+}
