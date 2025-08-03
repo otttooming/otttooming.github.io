39de51d2590e2a6f6125dfa23865d93e2a71553b
Ott Tooming <OttTooming@gmail.com>
2025-08-03 13:10:40 +0300

    refactor(HexGrid): use common tech map to iterate over

--- src/components/HexGrid/HexGrid.tsx
+++ src/components/HexGrid/HexGrid.tsx
@@ -2,46 +2,26 @@ import { Link } from '../Link/Link';
 import * as Logos from '../Logo/Logos';
 import * as styles from './HexGrid.css';
 
-interface HexProps {
-  children: React.ReactNode;
-  href: string;
-  variant: 'typescript' | 'redux' | 'gatsby' | 'react' | 'graphql';
-}
-
-const Hex = ({ children, href, variant }: HexProps) => (
-  <Link href={href} variant="plainExternal">
-    <span className={styles.hex({ variant })}>{children}</span>
-  </Link>
-);
+const Tech = [
+  ['https://www.typescriptlang.org/', Logos.TypeScript],
+  ['https://redux.js.org/', Logos.Redux],
+  ['https://www.gatsbyjs.org/', Logos.Gatsby],
+  ['https://reactjs.org/', Logos.React],
+  ['https://graphql.org/', Logos.GraphQL],
+] as const;
 
 const HexGrid = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.item}>
-        <Hex variant="typescript" href="https://www.typescriptlang.org/">
-          <Logos.TypeScript className={styles.icon} />
-        </Hex>
-      </div>
-      <div className={styles.item}>
-        <Hex variant="redux" href="https://redux.js.org/">
-          <Logos.Redux className={styles.icon} />
-        </Hex>
-      </div>
-      <div className={styles.item}>
-        <Hex variant="gatsby" href="https://www.gatsbyjs.org/">
-          <Logos.Gatsby className={styles.icon} />
-        </Hex>
-      </div>
-      <div className={styles.item}>
-        <Hex variant="react" href="https://reactjs.org/">
-          <Logos.React className={styles.icon} />
-        </Hex>
-      </div>
-      <div className={styles.item}>
-        <Hex variant="graphql" href="https://graphql.org/">
-          <Logos.GraphQL className={styles.icon} />
-        </Hex>
-      </div>
+      {Tech.map(([href, Icon]) => (
+        <div className={styles.item} key={Icon.name}>
+          <Link href={href} variant="plainExternal">
+            <span className={styles.hex({ variant: Icon.name })}>
+              <Icon className={styles.icon} />
+            </span>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
