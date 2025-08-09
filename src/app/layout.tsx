38b9792929e5340217977b79566be8386e0582a8
Ott Tooming <OttTooming@gmail.com>
2025-08-09 16:11:04 +0300

    fix(layout): simplify children prop type definition
    Updated the RootLayout component to use PropsWithChildren
    for the children prop type, enhancing readability and
    maintainability of the code.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer/Footer';
 import { ColorModeProvider } from '../components/ui/color-mode';
 import { Montserrat } from 'next/font/google';
 import { Viewport } from 'next';
+import { PropsWithChildren } from 'react';
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -17,11 +18,7 @@ const montserrat = Montserrat({
   subsets: ['latin'],
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={montserrat.className}>
