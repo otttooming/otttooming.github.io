70eb29686d8806de5724a6d7227fc96de02c6cf9
Ott Tooming <OttTooming@gmail.com>
2025-08-14 17:23:00 +0300

    refactor(HexGrid): clean up styles and remove unused properties
    - Removed marginTop from hexBase and wrapper styles.
    - Eliminated unused media query for marginTop.
    - Adjusted gap property in wrapper for better layout consistency.
    - Ensured styles are more streamlined and maintainable.

--- src/components/HexGrid/HexGrid.css.ts
+++ src/components/HexGrid/HexGrid.css.ts
@@ -1,4 +1,5 @@
 import { style, styleVariants } from '@vanilla-extract/css';
+import { theme } from '../../utils/theme.css';
 
 const hexBase = {
   display: 'flex',
@@ -8,15 +9,9 @@ const hexBase = {
   height: '16vw',
   maxWidth: '8rem',
   maxHeight: '8rem',
-  marginTop: '8px',
   transition: 'transform 200ms',
   clipPath:
     'polygon(45% 1.33975%, 46.5798% 0.60307%, 48.26352% 0.15192%, 50% 0%, 51.73648% 0.15192%, 53.4202% 0.60307%, 55% 1.33975%, 89.64102% 21.33975%, 91.06889% 22.33956%, 92.30146% 23.57212%, 93.30127% 25%, 94.03794% 26.5798%, 94.48909% 28.26352%, 94.64102% 30%, 94.64102% 70%, 94.48909% 71.73648%, 94.03794% 73.4202%, 93.30127% 75%, 92.30146% 76.42788%, 91.06889% 77.66044%, 89.64102% 78.66025%, 55% 98.66025%, 53.4202% 99.39693%, 51.73648% 99.84808%, 50% 100%, 48.26352% 99.84808%, 46.5798% 99.39693%, 45% 98.66025%, 10.35898% 78.66025%, 8.93111% 77.66044%, 7.69854% 76.42788%, 6.69873% 75%, 5.96206% 73.4202%, 5.51091% 71.73648%, 5.35898% 70%, 5.35898% 30%, 5.51091% 28.26352%, 5.96206% 26.5798%, 6.69873% 25%, 7.69854% 23.57212%, 8.93111% 22.33956%, 10.35898% 21.33975%)',
-  '@media': {
-    '(min-width: 960px)': {
-      marginTop: 0,
-    },
-  },
   selectors: {
     '&:hover': {
       transform: 'scale3d(1.2, 1.2, 1.2)',
@@ -34,19 +29,19 @@ export const hex = styleVariants<Record<string, unknown>>({
 });
 
 export const wrapper = style({
-  margin: '128px auto',
   display: 'flex',
   flexDirection: 'column-reverse',
+  gap: theme.space.s,
   '@media': {
     '(min-width: 960px)': {
       display: 'grid',
       gridTemplateAreas: `'. a1 a1 a2 a2 .' '. a1 a1 a2 a2 .' 'b1 b1 b2 b2 b3 b3' 'b1 b1 b2 b2 b3 b3' '. c1 c1 c2 c2 .' '. c1 c1 c2 c2 .'`,
+      gap: 0,
     },
   },
 });
 
 export const item = style({
-  position: 'relative',
   selectors: {
     '&:nth-of-type(1)': { gridArea: 'a1' },
     '&:nth-of-type(2)': { gridArea: 'a2' },
